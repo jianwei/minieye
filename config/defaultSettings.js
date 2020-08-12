cf6932638da59a6f9254ef248f4eb002a02d39ff
mac <chenjianwei@minieye.cc>
2020-08-12 15:35:06 +0800

    first commit

---
+++ config/defaultSettings.js
@@ -0,0 +1,19 @@
+const proSettings = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  splitMenus: true,
+  
+  menu: {
+    locale: true,
+  },
+  title: 'MINIEYE',
+  pwa: false,
+  iconfontUrl: '',
+};
+export default proSettings;
---
+++ src/pages/user/login/index.jsx
@@ -0,0 +1,72 @@
+import { AlipayCircleOutlined, TaobaoCircleOutlined, WeiboCircleOutlined } from '@ant-design/icons';
+import { Alert, Checkbox } from 'antd';
+import React, { useState } from 'react';
+import { Link, connect } from 'umi';
+import styles from './style.less';
+import LoginFrom from './components/Login';
+
+const { Tab, UserName, Password, Mobile, Captcha, Submit } = LoginFrom;
+
+const LoginMessage = ({ content }) => (
+  <Alert
+    style={{
+      marginBottom: 24,
+    }}
+    message={content}
+    type="error"
+    showIcon
+  />
+);
+
+const Login = props => {
+  const { userAndlogin = {}, submitting } = props;
+  const { status, type: loginType } = userAndlogin;
+  const [autoLogin, setAutoLogin] = useState(true);
+  const [type, setType] = useState('account');
+
+  const handleSubmit = values => {
+    const { dispatch } = props;
+    dispatch({
+      type: 'userAndlogin/login',
+      payload: { ...values, type },
+    });
+  };
+
+  return (
+    <div className={styles.main}>
+      <LoginFrom activeKey={type} onTabChange={setType} onSubmit={handleSubmit}>
+        {status === 'error' && loginType === 'account' && !submitting && (
+          <LoginMessage content="账户或密码错误（admin/ant.design）" />
+        )}
+
+        <UserName
+          name="userName"
+          placeholder="用户名: admin or user"
+          rules={[
+            {
+              required: true,
+              message: '请输入用户名!',
+            },
+          ]}
+        />
+        <Password
+          name="password"
+          placeholder="密码: ant.design"
+          rules={[
+            {
+              required: true,
+              message: '请输入密码！',
+            },
+          ]}
+        />
+
+        <Submit loading={submitting}>登录</Submit>
+      </LoginFrom>
+    </div>
+  );
+};
+
+export default connect(({ userAndlogin, loading }) => ({
+  userAndlogin,
+  submitting: loading.effects['userAndlogin/login'],
+}))(Login);
